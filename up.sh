a8c7e32caecd77ba7e9dce51a8075c75fa8cb7d7
BLACK_HYDRA <112259891+BlackHydra40@users.noreply.github.com>
2022-11-07 14:51:36 -0300

    Add files via upload

--- up.sh
+++ up.sh
@@ -4,14 +4,14 @@ rm -rf $HOME/BlackBIN
 figlet "ATUALIZANDO" | lolcat
 figlet "AGUARDE..." | lolcat
 figlet "ABRINDO GIT HUB" | lolcat
-am start -a android.intent.action.VIEW -d https://github.com/BlackHydra40/BlackBIN > /dev/null 2>&1
+am start -a android.intent.action.VIEW -d https://github.com/BlackHydra40 > /dev/null 2>&1
 sleep 5
 clear
 figlet "ABRINDO TELEGRAM" | lolcat
 am start -a android.intent.action.VIEW -d https://t.me/MS40_canal > /dev/null 2>&1
 sleep 3
 
-git clone https://github.com/BlackHydra40/BlackBIN
-cd BlackBIN
+git clone https://github.com/BlackHydra40/BlackBAN
+cd BlackBAN
 clear
 bash black.sh
